cd5be083395dbeb03d52b4bafaff288d45a60ba7
unknown <syidd.022@gmail.com>
2026-03-19 16:27:33 +0700

    Add Sheets URL

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ async function submitTransaksi() {
 
   // Kirim ke Google Sheets
   try {
-    if (APPS_SCRIPT_URL !== 'GANTI_DENGAN_URL_APPS_SCRIPT_KAMU') {
+    if (APPS_SCRIPT_URL !== '1u9vzHhyTdtZj4GvMbizSbCywEDIDJLUpbo8VWiwhO39Trk89LAMJb0gi') {
       await fetch(APPS_SCRIPT_URL, {
         method: 'POST',
         mode: 'no-cors',
